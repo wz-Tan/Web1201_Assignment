9a5f0f49a4ceb217a1955b6464d536eea4dc0b02
Yzutei <138152764+Yzutei@users.noreply.github.com>
2026-06-07 02:54:01 +0800

    feat: review display function

--- js/review.js
+++ js/review.js
@@ -49,6 +49,46 @@ for(let star of stars){
         this.setAttribute("data-clicked", "true");
         userRatings = this.dataset.rating;
         //test
-        console.log(userRatings)
+        console.log(userRatings);
     });
 }
+// form
+const reviewForm = document.getElementById('review-form');
+const reviewsList = document.getElementById('reviews-list');
+
+reviewForm.onsubmit = function(e){
+    e.preventDefault(); //this prevent website reset DO NOT REMOVE AGAIN
+
+    const product = document.getElementById('product-input').value;
+    const username = document.getElementById('username').value;
+    const rating = userRatings;
+    const review = document.getElementById('review').value;
+
+    const starRate = "\u2605".repeat(rating) + "\u2606".repeat(5-rating);
+
+    const newReview = document.createElement('div');
+    newReview.className = 'review-card';
+
+    newReview.innerHTML = `
+        <div class="review-card-header">
+            <h3 class="text-small text-bold text-accent">${product}</h3>
+            <div class="review-card-star">${starRate}</div>
+        </div>
+        <span class="review-card-user text-small">Reviewed by: ${username}</span>
+        <p class="review-card-text">${review}</p>`;
+
+    reviewsList.prepend(newReview);
+
+    reviewForm.reset();
+    userRatings=0;
+    stars.forEach(s => s.removeAttribute("data-clicked"));
+
+    //test
+    const formTest = {
+        product: document.getElementById('product-input').value,
+        rating: userRatings,
+        username: document.getElementById('username').value,
+        review: document.getElementById('review').value
+    };
+    console.log(formTest);
+};
