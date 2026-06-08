7d0f3c8e4508e94de8bcc4938a0afefe26bd7e7e
wz <wenzhe1423@gmail.com>
2026-06-08 21:39:39 +0800

    fix: theme glitches when pages first loads

--- js/theme.js
+++ js/theme.js
@@ -1,15 +1,6 @@
 // Purely For Theme Switching and Saving Themes
 const themeButton = document.querySelector(".btn-theme");
 
-// Load saved theme on page load (carry between pages)
-const savedTheme = localStorage.getItem("theme");
-if (savedTheme === "dark") {
-  document.documentElement.classList.add("dark");
-  themeButton.textContent = "☀️";
-} else {
-  themeButton.textContent = "🌙";
-}
-
 // Toggle current page, change local storage in the background
 themeButton.addEventListener("click", () => {
   const isDark = document.documentElement.classList.toggle("dark");
@@ -17,8 +8,9 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("theme", isDark ? "dark" : "light");
 });
 
-// First load doesnt trigger color transitions
+// First load doesnt trigger color transitions and button content
 window.addEventListener("load", () => {
-  console.log("Transitions now allowed");
   document.documentElement.classList.add("transition-allowed");
+  const savedTheme = localStorage.getItem("theme");
+  themeButton.textContent = savedTheme === "dark" ? "☀️" : "🌙";
 });
